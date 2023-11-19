609b87754141fc0276fe9582e08bf9347fa827d9
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-19 13:48:55 -0500

    wrote middleware and added vin-validator package

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,59 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require('./cars-model'); // Adjust the path as per your project structure
+const vinValidator = require('vin-validator');
+
+async function checkCarId(req, res, next) {
+  try {
+    const car = await Cars.getById(req.params.id);
+    if (!car) {
+      res.status(404).json({ message: `car with id ${req.params.id} is not found` });
+    } else {
+      req.car = car; // Add car to the request object for future use
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
-const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+function checkCarPayload(req, res, next) {
+  const { vin, make, model, mileage } = req.body;
+  if (!vin) {
+    return res.status(400).json({ message: "vin is missing" });
+  } else if (!make) {
+    return res.status(400).json({ message: "make is missing" });
+  } else if (!model) {
+    return res.status(400).json({ message: "model is missing" });
+  } else if (mileage === undefined) {
+    return res.status(400).json({ message: "mileage is missing" });
+  }
+  next();
 }
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+async function checkVinNumberValid(req, res, next) {
+  if (!vinValidator.validate(req.body.vin)) {
+    res.status(400).json({ message: `vin ${req.body.vin} is invalid` });
+  } else {
+    next();
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+async function checkVinNumberUnique(req, res, next) {
+  const existing = await Cars.getByVin(req.body.vin);
+  if (existing) {
+    res.status(400).json({ message: `vin ${req.body.vin} already exists` });
+  } else {
+    next();
+  }
 }
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+};
+
+
+
+
+
