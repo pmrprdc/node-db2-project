d6221f3630e80be9c170f755b364c362aa19bc2a
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-18 20:17:11 -0500

    configured server file to parse json and to use carsRouter on api/cars endpoints

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require("express")
+const carsRouter = require('./cars/cars-router'); // Replace with the actual path to your carsRouter file
 
 const server = express()
+
+server.use(express.json())
+
+server.use('/api/cars', carsRouter)
 
 // DO YOUR MAGIC
 
