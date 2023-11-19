7fa185370135dd15ad852e143c4542dddd86b8e3
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-18 20:12:23 -0500

    created migration functions to spec

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,16 @@
+
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {
+    table.increments('id'); // Creates an auto-incrementing primary key column named 'id'
+    table.string('vin').unique().notNullable(); // 'vin' column, unique and required
+    table.string('make').notNullable(); // 'make' column, required
+    table.string('model').notNullable(); // 'model' column, required
+    table.decimal('mileage').notNullable(); // 'mileage' column, required, using decimal for numeric values
+    table.string('title'); // 'title' column, optional
+    table.string('transmission'); // 'transmission' column, optional
+  });
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars'); // Drops the 'cars' table if it exists
 };
